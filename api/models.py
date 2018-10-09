d5f97839cb1aff9621f96027925320c962697db3
Saulo Lordão Andrade Barros <saulo.lordao@gmail.com>
2018-10-09 15:43:57 -0300

    Implemented first version of model classes.

--- api/models.py
+++ api/models.py
@@ -1,2 +1,51 @@
 from django.db import models
 
+
+class RiskType(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class RiskField(models.Model):
+    name = models.CharField(max_length=200)
+    api_type = models.TextField()
+    risk_type = models.ForeignKey(RiskType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - {}".format(self.risk_type.name, self.name)
+
+    class Meta:
+        ordering = ("risk_type", "name")
+
+
+class RiskEnums(models.Model):
+    """Possible values for a given enum RiskField."""
+    value = models.TextField()
+    field = models.ForeignKey(RiskField, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - ENUM {}".format(str(self.field), self.value)
+
+    class Meta:
+        ordering = ("field", "value")
+
+
+class RiskInstance(models.Model):
+    """Represents an instance of a RiskType."""
+    risk_type = models.ForeignKey(RiskType, on_delete=models.CASCADE)
+
+
+class RiskValues(models.Model):
+    """Represents the values of the fields a RiskInstance has."""
+    risk_object = models.ForeignKey(RiskInstance, on_delete=models.CASCADE)
+    risk_field = models.ForeignKey(RiskField, on_delete=models.CASCADE)
+    value = models.TextField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["risk_object"])
+        ]
+
+        unique_together = ("risk_object", "risk_field")
